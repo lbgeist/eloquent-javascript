23ceef18a77f4e087ca90761938d9e5ef2c34a47
lbgeist <40831989+lbgeist@users.noreply.github.com>
2019-07-27 08:56:11 -0400

    Adding files for chapter 4

---
+++ js/chapter-4/deep-comparison.js
@@ -0,0 +1,29 @@
+// Your code here. I don't get this one either. 
+//I really feel like all these books, etc. don't give enough information. 
+//And I struggle with recursiveness. I really really do. 
+// below is the author's solution. Definitely not mine. 
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
---
+++ js/chapter-4/reversing-an-array.js
@@ -0,0 +1,49 @@
+// Your code here.
+let reverseArray = function(array) {
+	let newArray = [];
+	for (i = array.length - 1; i >= 0; i--) {
+		newArray.push(array[i]);
+	}
+	return newArray;
+}
+
+let reverseArrayInPlace = function(array) {
+
+}
+
+
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+
+
+/*
+Author's answer: 
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+*/
